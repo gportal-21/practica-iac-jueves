a182769dafa24762b766351c4d911d6fce6905a6
Gerson Ivan Portal Linares <gportall1@upao.edu.pe>
2026-05-04 09:59:40 -0500

    Feature/api gateway (#1)
    * feat: configuración de cors en Gateway
    
    * feat: Creacion de recurso para integracion con upload-lambda
    
    * feat: Creacion de recurso para una route_key especifica
    
    * feat: Creacion de stage por default
    
    * feat: creacion de recurso permission para ejecutar el lambda con api-gateway
    
    ---------
    
    Co-authored-by: Gerson Portal <gportallinares21@gmail.com>

---
+++ iac/api-gateway.tf
@@ -0,0 +1,65 @@
+resource "aws_apigatewayv2_api" "api_gateway" {
+  name          = "api-gateway-${terraform.workspace}"
+  protocol_type = "HTTP"
+
+  cors_configuration {
+    allow_origins = ["*"]
+    allow_methods = ["POST", "OPTIONS"]
+    allow_headers = ["content-type"]
+    max_age       = 300
+  }
+}
+
+
+resource "aws_apigatewayv2_integration" "upload_lambda" {
+  api_id           = aws_apigatewayv2_api.api_gateway.id
+  integration_type = "AWS_PROXY"
+
+  connection_type        = "INTERNET"
+  description            = "Upload file to S3 bucket"
+  integration_method     = "POST"
+  integration_uri        = aws_lambda_function.upload_lambda.invoke_arn
+  timeout_milliseconds   = 30000
+  payload_format_version = "2.0"
+}
+
+resource "aws_apigatewayv2_route" "post_upload" {
+  api_id    = aws_apigatewayv2_api.api_gateway.id
+  route_key = "POST /upload"
+  target    = "integrations/${aws_apigatewayv2_integration.upload_lambda.id}"
+}
+
+resource "aws_apigatewayv2_stage" "default" {
+  api_id      = aws_apigatewayv2_api.api_gateway.id
+  name        = "$default"
+  auto_deploy = true
+
+  default_route_settings {
+    throttling_rate_limit    = 10
+    throttling_burst_limit   = 20
+    detailed_metrics_enabled = true
+  }
+
+  access_log_settings {
+    destination_arn = aws_cloudwatch_log_group.apigw_access.arn
+    format = jsonencode({
+      requestId        = "$context.requestId"
+      ip               = "$context.identity.sourceIp"
+      requestTime      = "$context.requestTime"
+      httpMethod       = "$context.httpMethod"
+      routeKey         = "$context.routeKey"
+      status           = "$context.status"
+      protocol         = "$context.protocol"
+      responseLength   = "$context.responseLength"
+      integrationError = "$context.integrationErrorMessage"
+    })
+  }
+}
+
+resource "aws_lambda_permission" "apigw_invoke_upload_lambda" {
+  statement_id  = "AllowAPIGatewayInvokeUploadLambda-${terraform.workspace}"
+  action        = "lambda:InvokeFunction"
+  function_name = aws_lambda_function.upload_lambda.upload_image
+  principal     = "apigateway.amazonaws.com"
+  source_arn    = "${aws_apigatewayv2_api.api_gateway.execution_arn}/*/POST/upload" # Se puede usar /*/* porque solo hay una ruta creada
+}
