2ead11757e848eacd9dd22d1dd53a1f1aa338553
Gerson Portal <gportallinares21@gmail.com>
2026-05-05 16:20:17 -0500

    feat: Creacion de policies para denegar trafico Https externo

--- iac/storage.tf
+++ iac/storage.tf
@@ -84,3 +84,31 @@ resource "aws_s3_bucket_lifecycle_configuration" "images" {
 
   depends_on = [aws_s3_bucket_versioning.images]
 }
+
+resource "aws_s3_bucket_policy" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid       = "DenyInsecureTransport"
+        Effect    = "Deny"
+        Principal = "*"
+        Action    = "s3:*"
+        Resource = [
+          aws_s3_bucket.images.arn,
+          "${aws_s3_bucket.images.arn}/*"
+        ]
+        Condition = {
+          Bool = {
+            "aws:SecureTransport" = "false"
+          }
+        }
+      }
+    ]
+  })
+
+  # Sin esto, race condition con public_access_block
+  depends_on = [aws_s3_bucket_public_access_block.images]
+}
