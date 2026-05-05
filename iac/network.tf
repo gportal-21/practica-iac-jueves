21fca29234a11f663890a5bc4bb0d70c2b253444
Gerson Portal <gportallinares21@gmail.com>
2026-05-05 10:52:23 -0500

    feat: Creacion de routes faltantes para nat-gateway

--- iac/network.tf
+++ iac/network.tf
@@ -90,12 +90,24 @@ resource "aws_route_table" "private_a" {
   }
 }
 
+resource "aws_route" "private_a_nat" {
+  route_table_id         = aws_route_table.private_a.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_a.id
+}
+
 resource "aws_route_table" "private_b" {
   vpc_id = aws_vpc.main.id
 
   tags = {
     Name = "image-processor-rt-private-b-${terraform.workspace}"
   }
+}
+
+resource "aws_route" "private_b_nat" {
+  route_table_id         = aws_route_table.private_b.id
+  destination_cidr_block = "0.0.0.0/0"
+  nat_gateway_id         = aws_nat_gateway.nat_b.id
 }
 
 resource "aws_route_table_association" "public_a" {
