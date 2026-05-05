051aa1477c0f081382f534f59571fb5d71f45103
Gerson Portal <gportallinares21@gmail.com>
2026-05-04 21:53:25 -0500

    feat: Creacion de subredes publicas y privadas

--- iac/network.tf
+++ iac/network.tf
@@ -16,3 +16,45 @@ resource "aws_internet_gateway" "main" {
     Name = "image-processor-igw-${terraform.workspace}"
   }
 }
+
+resource "aws_subnet" "public_a" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.1.0/24"
+  availability_zone       = "us-east-2a"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "image-processor-public-a-${terraform.workspace}"
+  }
+}
+
+resource "aws_subnet" "public_b" {
+  vpc_id                  = aws_vpc.main.id
+  cidr_block              = "10.0.2.0/24"
+  availability_zone       = "us-east-2b"
+  map_public_ip_on_launch = true
+
+  tags = {
+    Name = "image-processor-public-b-${terraform.workspace}"
+  }
+}
+
+resource "aws_subnet" "private_a" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.11.0/24"
+  availability_zone = "us-east-2a"
+
+  tags = {
+    Name = "image-processor-private-a-${terraform.workspace}"
+  }
+}
+
+resource "aws_subnet" "private_b" {
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = "10.0.12.0/24"
+  availability_zone = "us-east-2b"
+
+  tags = {
+    Name = "image-processor-private-b-${terraform.workspace}"
+  }
+}
