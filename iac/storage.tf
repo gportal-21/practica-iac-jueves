7c0d53365456f7ef02f6271ee9418df571cb76aa
Gerson Portal <gportallinares21@gmail.com>
2026-05-05 15:47:56 -0500

    feat: Creacion de bucket s3 con versionamiento, escriptacion, access block y ownership

---
+++ iac/storage.tf
@@ -0,0 +1,42 @@
+resource "aws_s3_bucket" "images" {
+  bucket_prefix = "image-processor-${terraform.workspace}-images-"
+
+  tags = {
+    Name = "image-processor-images-${terraform.workspace}"
+  }
+}
+
+resource "aws_s3_bucket_versioning" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_s3_bucket_server_side_encryption_configuration" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  rule {
+    apply_server_side_encryption_by_default {
+      sse_algorithm = "AES256"
+    }
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
+}
+
+resource "aws_s3_bucket_ownership_controls" "images" {
+  bucket = aws_s3_bucket.images.id
+
+  rule {
+    object_ownership = "BucketOwnerEnforced"
+  }
+}
