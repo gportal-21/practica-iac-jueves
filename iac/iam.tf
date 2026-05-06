21d20ba875f9523ed22b864ce237829fb0857449
Gerson Portal <gportallinares21@gmail.com>
2026-05-05 19:38:56 -0500

    feat: Creacion de politicas y roles para lambdas

---
+++ iac/iam.tf
@@ -0,0 +1,29 @@
+data "aws_iam_policy_document" "lambda_trust" {
+  statement {
+    effect  = "Allow"
+    actions = ["sts:AssumeRole"]
+
+    principals {
+      type        = "Service"
+      identifiers = ["lambda.amazonaws.com"]
+    }
+  }
+}
+
+resource "aws_iam_role" "upload_lambda" {
+  name               = "upload-lambda-role-${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+
+  tags = {
+    Name = "upload-lambda-role-${terraform.workspace}"
+  }
+}
+
+resource "aws_iam_role" "crop_lambda" {
+  name               = "crop-lambda-role-${terraform.workspace}"
+  assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
+
+  tags = {
+    Name = "crop-lambda-role-${terraform.workspace}"
+  }
+}
