46601645c9e87fd452991732bb1214174aad176d
Gerson Portal <gportallinares21@gmail.com>
2026-05-05 20:01:46 -0500

    feat: Creacion de politicas para upload vpc, lambda y s3 (PUT)

--- iac/iam.tf
+++ iac/iam.tf
@@ -19,6 +19,31 @@ resource "aws_iam_role" "upload_lambda" {
   }
 }
 
+resource "aws_iam_role_policy_attachment" "upload_basic" {
+  role       = aws_iam_role.upload_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_iam_role_policy_attachment" "upload_vpc" {
+  role       = aws_iam_role.upload_lambda.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaVPCAccessExecutionRole"
+}
+
+data "aws_iam_policy_document" "upload_s3" {
+  statement {
+    sid       = "AllowPutObjectInUploadsPrefix"
+    effect    = "Allow"
+    actions   = ["s3:PutObject"]
+    resources = ["${aws_s3_bucket.images.arn}/uploads/*"]
+  }
+}
+
+resource "aws_iam_role_policy" "upload_s3" {
+  name   = "upload-lambda-s3-policy"
+  role   = aws_iam_role.upload_lambda.id
+  policy = data.aws_iam_policy_document.upload_s3.json
+}
+
 resource "aws_iam_role" "crop_lambda" {
   name               = "crop-lambda-role-${terraform.workspace}"
   assume_role_policy = data.aws_iam_policy_document.lambda_trust.json
